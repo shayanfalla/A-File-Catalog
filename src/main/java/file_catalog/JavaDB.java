d20f8c822c65c39a2716643df1b5b3d2506d5b74
Shayan Fallahian <shayanf@kth.se>
2017-11-30 20:29:42 +0100

    committzz

--- src/main/java/file_catalog/JavaDB.java
+++ src/main/java/file_catalog/JavaDB.java
@@ -14,14 +14,52 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package file_catalog;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class JavaDB {
+
+    private static final String TABLE_NAME = "filename";
+
+    private void DataAccess() {
+        try {
+            Class.forName("org.apache.derby.jdbc.ClientXADataSource");
+            Connection connection = DriverManager.getConnection("jdbc:derby://localhost:1527//File-Catalog", "file", "file");
+            createTable(connection);
+        } catch (ClassNotFoundException | SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
     
-    public static void main(String [] args){
-        int i = 1;
+    private void createTable(Connection connection) throws SQLException{
+       if (!tableExists(connection)) {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(
+                    "create table " + TABLE_NAME + " (filename varchar(32) primary key, owner varchar(32), permission varchar(32), readwrite varchar(32))");
+        }
     }
 
+    private boolean tableExists(Connection connection) throws SQLException {
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet tableMetaData = metaData.getTables(null, null, null, null);
+        while (tableMetaData.next()) {
+            String tableName = tableMetaData.getString(3);
+            if (tableName.equalsIgnoreCase(TABLE_NAME)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        new JavaDB().DataAccess();
+    }
 }
